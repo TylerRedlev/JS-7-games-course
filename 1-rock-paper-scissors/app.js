8a4aade217f9ad70308ea27ddaaa717d5b369116
Durukan Kizilarslan <tyler.redlev@gmail.com>
2023-12-29 17:27:05 +0300

    Added event listener to userChoice buttons

--- 1-rock-paper-scissors/app.js
+++ 1-rock-paper-scissors/app.js
@@ -1,16 +1,16 @@
 //Declaring elements data
-const computerChoice = document.getElementById("computer-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
-const possibleChoices = document.querySelector("button");
+const possibleChoices = document.querySelectorAll("button");
 let userChoice;
 
+console.log(possibleChoices);
+
 //Add event listener to possible choices buttons
-possibleChoices.forEach(
-  (possibleChoice) => {
-    console.log(possibleChoice);
-  }
-  // possibleChoice.addEventListener("click", (e) => {
-  //   userChoice = e.target.id;
-  // })
-);
+possibleChoices.forEach((possibleChoice) => {
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+  });
+});
