e1c28fe37be5084998990a8b3229d634bcadbdc5
Durukan Kizilarslan <tyler.redlev@gmail.com>
2024-01-01 13:19:55 +0300

    Get result function added

--- 1-rock-paper-scissors/app.js
+++ 1-rock-paper-scissors/app.js
@@ -4,6 +4,8 @@ const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
+let computerChoice;
+let result;
 
 console.log(possibleChoices);
 
@@ -11,12 +13,37 @@ console.log(possibleChoices);
 possibleChoices.forEach((possibleChoice) => {
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
-    userChoiceDisplay.innerHTML = userChoice;
+    userChoiceDisplay.innerHTML =
+      userChoice.charAt(0).toUpperCase() + userChoice.slice(1);
     generateComputerChoice();
+    getResult();
   });
 });
 
+//Adding generated computer choice
 function generateComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1; //or you can use possibleChoices.length
   console.log(randomNumber);
+
+  if (randomNumber === 1) {
+    computerChoice = "Rock";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "Paper";
+  }
+  if (randomNumber === 3) {
+    computerChoice = "Scissors";
+  }
+
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+//Add result function
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "It's a draw!";
+  }
+  if (computerChoice === "Rock" && userChoice === "Scissors") {
+    result = "You win!";
+  }
 }
