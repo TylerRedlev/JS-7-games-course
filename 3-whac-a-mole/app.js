01818faf03f2a958545a7f235a8009504c91fa36
Durukan Kizilarslan <tyler.redlev@gmail.com>
2024-01-07 01:32:17 +0300

    WHACK-A-MOLE game completed

--- 3-whac-a-mole/app.js
+++ 3-whac-a-mole/app.js
@@ -1,9 +1,40 @@
+//==============================
+//DECLARING CONSTANTS AND VARIABLES
+//==============================
+
+//Declaring constants and variables
 const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
 const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPosition;
+let currentTime = 10;
+let countDownTimerId = setInterval(countDown, 1000);
+let timerId;
+
+//==============================
+//START THE PROGRAM
+//==============================
+
+//Adding event listeners to squares
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+//Starting the move of the mole
+moveMole();
+
+//==============================
+//DECLARING THE FUNCTIONS
+//==============================
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,11 +43,21 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * squares.length)];
   randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
 }
 
 function moveMole() {
-  let timerId = null;
+  timerId = null;
   timerId = setInterval(randomSquare, 500);
 }
 
-moveMole();
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert(`GAME OVER! Your final score is ${result}`);
+  }
+}
