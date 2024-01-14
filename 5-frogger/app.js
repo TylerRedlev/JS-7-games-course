de88711d94ec4768da01ac522eb781e6f921ba09
Durukan Kizilarslan <tyler.redlev@gmail.com>
2024-01-14 17:23:04 +0300

    base constants added

---
+++ 5-frogger/app.js
@@ -0,0 +1,10 @@
+const timeleftDisplay = document.querySelector("#time-left");
+const resultDisplay = document.querySelector("#result");
+const startPauseButton = document.querySelector("#start-pause-button");
+const squares = document.querySelectorAll(".grid div");
+
+function moveFrog() {
+  console.log("move");
+}
+
+document.addEventListener("keyup", moveFrog);
